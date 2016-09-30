331b7c2307f34bef8b313cba036b4448203ec5ed
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-30 20:14:29 +0700

    Initial commit

---
+++ lib/App/numspeed.pm
@@ -0,0 +1,18 @@
+package App::numspeed;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Calculate how fast a number is changing
+
+=head1 SYNOPSIS
+
+See the command-line script L<numspeed>.
+
+
+=head1 SEE ALSO
+
+L<linespeed> from L<App::linespeed>
+
+=cut
